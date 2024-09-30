98643b4819b44eddb43233cd2c41ba2e5e222a89
lunarplums <lunarplums@protonmail.com>
2024-09-30 05:29:41 -0500

    Attempts at fixing some things

---
+++ Windows Standard style/linkgtkfolders.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ln -sf "$HOME/.local/share/themes/Windows Standard style/Resources" $HOME/.config/ &
+
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/assistant.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/buttons.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/checks_radio.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/combobox.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/entry.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/frames.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/gtk.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/headerbars.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/list_cell_and_rows.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/menu.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/messagedialog.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/notebooks.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/paned.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/popovers.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/progressbar.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/scrollbars.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/scrolledwindows.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/separators.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/titlebars.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/toolbar.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/treeviews.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/viewports.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/widgets.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/window.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/windowbuttons.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/wintc.css" $HOME/.config/gtk-3.0/ &
+ln -sf "$HOME/.local/share/themes/Windows Standard style/gtk-3.0/xfce.css" $HOME/.config/gtk-3.0/
